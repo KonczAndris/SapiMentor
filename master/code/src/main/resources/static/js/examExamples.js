d626f12577372a7be62d6411e3fe0b27a671c6b9
KonczAndris <koncz.andras@student.ms.sapientia.ro>
2023-08-28 13:21:36 +0300

    Update
    Resources Menu Design Phone Stlye

--- master/code/src/main/resources/static/js/examExamples.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("examExamples").addEventListener("click", function() {
-    var examExamplesUrl = /*[[ @{/examExamples} ]]*/ '';
-    window.location.href = examExamplesUrl;
-});
-
-document.getElementById("diplomaTheses").addEventListener("click", function() {
-    var diplomaThesesUrl = /*[[ @{/diplomaTheses} ]]*/ '';
-    window.location.href = diplomaThesesUrl;
-});
--- master/code/src/main/resources/static/js/resources.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("diplomaTheses").addEventListener("click", function() {
-    window.location.href = "diplomaTheses.html";
-});
-
-document.getElementById("diplomaTheses").addEventListener("click", function() {
-    var diplomaThesesUrl = /*[[ @{/diplomaTheses} ]]*/ '';
-    window.location.href = diplomaThesesUrl;
-});
