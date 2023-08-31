8011a9f08678eef3b970c8fdd16700b634721893
KonczAndris <koncz.andras@student.ms.sapientia.ro>
2023-08-31 10:14:31 +0300

    Update
    Resources Page Menu Design Correcting

--- master/code/target/classes/static/js/diplomaTheses.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("resources").addEventListener("click", function() {
-    var resourcesUrl = /*[[ @{/resources} ]]*/ '';
-    window.location.href = resourcesUrl;
-});
-
-document.getElementById("examExamples").addEventListener("click", function() {
-    var examExamplesUrl = /*[[ @{/examExamples} ]]*/ '';
-    window.location.href = examExamplesUrl;
-});
