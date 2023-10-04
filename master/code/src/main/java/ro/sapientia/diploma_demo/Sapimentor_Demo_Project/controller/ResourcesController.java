0fbcfd7d436aebfabb7eaf018d6f312d6a221bc9
Szotyori Csongor-Botond <szotyori.csongor@student.ms.sapientia.ro>
2023-10-04 20:30:45 +0300

    Update
    Some functions in resources page

--- master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/controller/ResourcesController.java
+++ master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/controller/ResourcesController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ro.sapientia.diploma_demo.Sapimentor_Demo_Project.model.Role;
+import ro.sapientia.diploma_demo.Sapimentor_Demo_Project.model.Topic;
 import ro.sapientia.diploma_demo.Sapimentor_Demo_Project.model.User;
 import ro.sapientia.diploma_demo.Sapimentor_Demo_Project.repository.UserRepository;
+import ro.sapientia.diploma_demo.Sapimentor_Demo_Project.service.TopicService;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -15,10 +17,13 @@ import java.util.List;
 @Controller
 public class ResourcesController {
     private final UserRepository userRepository;
+    private final TopicService topicService;
 
     @Autowired
-    public ResourcesController(UserRepository userRepository) {
+    public ResourcesController(UserRepository userRepository,
+                               TopicService topicService) {
         this.userRepository = userRepository;
+        this.topicService = topicService;
     }
 
     private void showUserRolesToDisplayResources(Model model, Principal principal){
@@ -42,12 +47,23 @@ public class ResourcesController {
         }
 
         String rolesAsString = String.join(",", rolesToDisplayResources);
+        System.out.println("Roles: " + rolesAsString);
         model.addAttribute("userRolesToDisplayResources", rolesAsString);
+    }
+
+    private void showTopicsToDisplayResources(Model model, Principal principal){
+        String email = principal.getName();
+        User user = userRepository.findByEmail(email);
+        // Itt lekérem a témákat a service segítségével
+        List<Topic> topics = topicService.getAllTopics();
+        System.out.println("Topics: " + topics);
+        model.addAttribute("topics", topics);
     }
 
     @GetMapping("/resources")
     public String showResources(Model model, Principal principal) {
         showUserRolesToDisplayResources(model, principal);
+        showTopicsToDisplayResources(model, principal);
         return "resources";
     }
     @GetMapping("/resources/examexamples")
