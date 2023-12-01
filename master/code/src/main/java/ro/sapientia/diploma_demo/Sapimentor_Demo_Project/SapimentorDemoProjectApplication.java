7e0eb10c156a5c072a305dc6dcaa1841f29c28be
Szotyori Csongor-Botond <szotyori.csongor@student.ms.sapientia.ro>
2023-12-01 18:29:01 +0200

    Update
    Upload some pdf with Command Line Runner

--- master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/SapimentorDemoProjectApplication.java
+++ master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/SapimentorDemoProjectApplication.java
@@ -12,4 +12,105 @@ public class SapimentorDemoProjectApplication {
 		System.out.println("Run Successfully!");
 	}
 
+	private static final Long MAX_PDF_FILE_SIZE = 10 * 1024 * 1024L; // 10 MB
+
+	// Informatika szakos diakok diplomamunkainak feltoltese
+	// a CommandLineRunner interfesz implementalas segitsegevel
+//	@Bean
+//	public CommandLineRunner InformaticPDFUploadRunner(DiplomaServices diplomaServices) {
+//		return args -> {
+//			String pdfDirectoryPath = "src/main/resources/static/pdf";
+//
+//			// PDF-fájlok listázása a könyvtárban
+//			File pdfDirectory = new File(pdfDirectoryPath);
+//			File[] pdfFiles = pdfDirectory.listFiles((dir, name) -> name.toLowerCase().endsWith(".pdf"));
+//
+//			if (pdfFiles != null) {
+//				for (File pdfFile : pdfFiles) {
+//					String originalName = pdfFile.getName().replace(".pdf", "");
+//					String cleanedName = cleanFileName(originalName);
+//					String formattedName = formatName(cleanedName);
+//					String topic = "Informatics";
+//					String user_Name = "Szotyori Csongor";
+//					String year = "2023";
+//
+//
+//					if (pdfFile.length() > MAX_PDF_FILE_SIZE) {
+//						System.out.println("PDF file " + formattedName + " is larger than 10MB. Skipping...");
+//						continue;
+//					}
+//					byte[] pdfBytes = Files.readAllBytes(pdfFile.toPath());
+//					//System.out.println("PDF file name: " + formattedName + " | PDF file size: " + pdfFile.length() + " bytes");
+//					//System.out.println(pdfFile.getName().replace(".pdf", ""));
+//
+//					Thread.sleep(6000);
+//					System.out.println("PDF file " + formattedName + " is smaller than 10MB. Uploading...");
+//					diplomaServices.uploadDiplomaThesesPdfByCLR(pdfBytes, formattedName, topic, user_Name, year);
+//				}
+//			}
+//
+//			//System.out.println("PDF files found in the directory: " + pdfFiles.length);
+//			System.out.println("CommandLineRunner running in the UnsplashApplication class...");
+//		};
+//	}
+
+
+	// Szamitastechnika szakos diakok diplomamunkainak feltoltese
+	// a CommandLineRunner interfesz implementalas segitsegevel
+//	@Bean
+//	public CommandLineRunner CalculatoarePDFUploadRunner(DiplomaServices diplomaServices) {
+//		return args -> {
+//			String pdfDirectoryPath = "src/main/resources/static/pdf";
+//
+//			// PDF-fájlok listázása a könyvtárban
+//			File pdfDirectory = new File(pdfDirectoryPath);
+//			File[] pdfFiles = pdfDirectory.listFiles((dir, name) -> name.toLowerCase().endsWith(".pdf"));
+//
+//			if (pdfFiles != null) {
+//				for (File pdfFile : pdfFiles) {
+//					String originalName = pdfFile.getName().replace(".pdf", "");
+//					String cleanedName = cleanFileName(originalName);
+//					String formattedName = formatName(cleanedName);
+//					String topic = "Engineering";
+//					String user_Name = "Koncz Andras";
+//					String year = "2023";
+//
+//
+//					if (pdfFile.length() > MAX_PDF_FILE_SIZE) {
+//						System.out.println("PDF file " + formattedName + " is larger than 10MB. Skipping...");
+//						continue;
+//					}
+//					byte[] pdfBytes = Files.readAllBytes(pdfFile.toPath());
+//					//System.out.println("PDF file name: " + formattedName + " | PDF file size: " + pdfFile.length() + " bytes");
+//					//System.out.println(pdfFile.getName().replace(".pdf", ""));
+//
+//					Thread.sleep(3000);
+//					System.out.println("PDF file " + formattedName + " is smaller than 10MB. Uploading...");
+//					diplomaServices.uploadDiplomaThesesPdfByCLR(pdfBytes, formattedName, topic, user_Name, year);
+//				}
+//			}
+//
+//			//System.out.println("PDF files found in the directory: " + pdfFiles.length);
+//			System.out.println("CommandLineRunner running in the UnsplashApplication class...");
+//		};
+//	}
+
+	private String cleanFileName(String fileName) {
+		// Az "_" és "-" karakterek eltávolítása
+		return fileName.replaceAll("[_-]", "");
+	}
+
+	private String formatName(String name) {
+		// "Diplomadolgozat" és "2023" szavak eltávolítása
+		name = name.replace("Diplomadolgozat", "").replace("2023", "");
+
+		// Név formázása "Nagy Marton Hunor" formátummá
+		String[] nameParts = name.split("(?=[A-Z])");
+		StringBuilder formattedName = new StringBuilder();
+		for (String part : nameParts) {
+			formattedName.append(part).append(" ");
+		}
+		return formattedName.toString().trim();
+	}
+
 }
--- master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/service/DiplomaServices.java
+++ master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/service/DiplomaServices.java
@@ -101,70 +101,9 @@ public class DiplomaServices {
     //private static final long MAX_PDF_SIZE = 5 * 1024 * 1024; // 5 MB
     //private static final long MAX_PDF_SIZE = 3 * 1024 * 1024; // 3 MB
 
-//    public String uploadDiplomaThesesPdf(MultipartFile pdf,
-//                                         String name,
-//                                         String topic,
-//                                         String user_name){
-//        if(!pdf.isEmpty()) {
-//            try {
-//                System.out.println("Pdf size: " + pdf.getSize());
-//                System.out.println("MAX_PDF_SIZE: " + MAX_PDF_SIZE);
-//                System.out.println("Pdf name: " + name);
-//                System.out.println("Pdf topic: " + topic);
-//                System.out.println("Pdf user_name: " + user_name);
-//
-//                if (pdf.getSize() > MAX_PDF_SIZE) {
-//                    return "Too large";
-//                }
-//
-//                byte[] originalPdfBytes = pdf.getBytes();
-//
-//                // Először alakítsd át InputStream-re
-//                ByteArrayInputStream pdfInputStream = new ByteArrayInputStream(originalPdfBytes);
-//                // Tömörítés előkészítése iText segítségével
-//                ByteArrayOutputStream compressedPdfStream = new ByteArrayOutputStream();
-//
-//                PdfReader pdfReader = new PdfReader(pdfInputStream);
-//                PdfWriter pdfWriter = new PdfWriter(compressedPdfStream);
-//
-//                PdfDocument pdfDocument = new PdfDocument(pdfReader, pdfWriter);
-//
-//                // Itt végrehajthatod a tömörítési műveleteket, például a képek minőségének csökkentését
-//                // A képek minőségének csökkentése 80%-ra
-//                int numPages = pdfDocument.getNumberOfPages();
-//                for (int i = 1; i <= numPages; i++) {
-//                    pdfDocument.getPage(i).getPdfObject().setCompressionLevel(9);
-//                }
-//
-//                pdfDocument.close();
-//                pdfReader.close();
-//
-//                byte[] compressedPdfBytes = compressedPdfStream.toByteArray();
-//
-//                // itt hozom letre a Diploma_Theses objektumot
-//                // es teszem bele a megadott adatokat
-//                Diploma_Theses diploma_theses = new Diploma_Theses();
-//                diploma_theses.setName(name);
-//                diploma_theses.setTopic_name(topic);
-//                diploma_theses.setUser_name(user_name);
-//                diploma_theses.setLike(0);
-//                diploma_theses.setDislike(0);
-//
-////                // itt adom hozzá a tömörítetlen PDF-et az objektumhoz
-////                diploma_theses.setDiploma_theses_file(originalPdfBytes);
-//
-//                // Tömörített PDF hozzáadása az objektumhoz
-//                diploma_theses.setDiploma_theses_file(compressedPdfBytes);
-//
-//                //diplomaThesesRepository.save(diploma_theses);
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        return null;
-//    }
 
+
+// oldalon a felhasznalo altal hasznalt metodus
 public String uploadDiplomaThesesPdf(MultipartFile pdf,
                                      String name,
                                      String topic,
@@ -172,11 +111,11 @@ public String uploadDiplomaThesesPdf(MultipartFile pdf,
                                      String year){
     if(!pdf.isEmpty()) {
         try {
-            System.out.println("Pdf size: " + pdf.getSize());
-            System.out.println("MAX_PDF_SIZE: " + MAX_PDF_SIZE);
-            System.out.println("Pdf name: " + name);
-            System.out.println("Pdf topic: " + topic);
-            System.out.println("Pdf user_name: " + user_name);
+//            System.out.println("Pdf size: " + pdf.getSize());
+//            System.out.println("MAX_PDF_SIZE: " + MAX_PDF_SIZE);
+//            System.out.println("Pdf name: " + name);
+//            System.out.println("Pdf topic: " + topic);
+//            System.out.println("Pdf user_name: " + user_name);
 
             if (pdf.getSize() > MAX_PDF_SIZE) {
                 return "Too large";
@@ -239,6 +178,81 @@ public String uploadDiplomaThesesPdf(MultipartFile pdf,
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    return null;
+}
+
+// a CommandLineRunner altal hasznalt metodus
+public String uploadDiplomaThesesPdfByCLR(byte[] pdfBytes,
+                                     String name,
+                                     String topic,
+                                     String user_name,
+                                     String year){
+    try {
+//        System.out.println("Pdf size: " + pdfBytes.length);
+//        System.out.println("MAX_PDF_SIZE: " + MAX_PDF_SIZE);
+//        System.out.println("Pdf name: " + name);
+//        System.out.println("Pdf topic: " + topic);
+//        System.out.println("Pdf user_name: " + user_name + "\n");
+
+        if (pdfBytes.length > MAX_PDF_SIZE) {
+            return "Too large";
+        }
+
+        // Először alakítsd át InputStream-re
+        ByteArrayInputStream pdfInputStream = new ByteArrayInputStream(pdfBytes);
+        // Tömörítés előkészítése iText segítségével
+        ByteArrayOutputStream compressedPdfStream = new ByteArrayOutputStream();
+
+        PdfReader pdfReader = new PdfReader(pdfInputStream);
+        PdfWriter pdfWriter = new PdfWriter(compressedPdfStream);
+
+        PdfDocument pdfDocument = new PdfDocument(pdfReader, pdfWriter);
+
+        // Itt végrehajthatod a tömörítési műveleteket, például a képek minőségének csökkentését
+        // A képek minőségének csökkentése 80%-ra
+        int numPages = pdfDocument.getNumberOfPages();
+        for (int i = 1; i <= numPages; i++) {
+            PdfPage page = pdfDocument.getPage(i);
+            PdfDictionary resources = page.getPdfObject().getAsDictionary(PdfName.Resources);
+            if (resources != null) {
+                PdfDictionary xObject = resources.getAsDictionary(PdfName.XObject);
+                if (xObject != null) {
+                    PdfDictionary imageDict = xObject.getAsDictionary(PdfName.Image);
+                    if (imageDict != null) {
+                        imageDict.put(PdfName.Filter, PdfName.DCTDecode);
+                    }
+                }
+            }
+        }
+
+        pdfDocument.close();
+        pdfReader.close();
+
+        byte[] compressedPdfBytes = compressedPdfStream.toByteArray();
+
+        // System.out.println("Compressed PDF size: " + compressedPdfBytes.length);
+
+        // itt hozom letre a Diploma_Theses objektumot
+        // es teszem bele a megadott adatokat
+        Diploma_Theses diploma_theses = new Diploma_Theses();
+        diploma_theses.setName(name);
+        diploma_theses.setTopic_name(topic);
+        diploma_theses.setUser_name(user_name);
+        diploma_theses.setYear(year);
+        diploma_theses.setLike(0);
+        diploma_theses.setDislike(0);
+
+//            // itt adom hozzá a tömörítetlen PDF-et az objektumhoz
+//            diploma_theses.setDiploma_theses_file(originalPdfBytes);
+
+        // Tömörített PDF hozzáadása az objektumhoz
+        diploma_theses.setDiploma_theses_file(compressedPdfBytes);
+
+       diplomaThesesRepository.save(diploma_theses);
+
+    } catch (Exception e) {
+        e.printStackTrace();
     }
     return null;
 }
