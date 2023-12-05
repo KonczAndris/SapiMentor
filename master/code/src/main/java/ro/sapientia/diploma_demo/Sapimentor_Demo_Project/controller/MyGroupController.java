6a376aa2db624a0c92c6ae48596ef0341a778919
Szotyori Csongor-Botond <szotyori.csongor@student.ms.sapientia.ro>
2023-12-05 18:27:03 +0200

    Update

--- master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/controller/MyGroupController.java
+++ master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/controller/MyGroupController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,11 +21,18 @@ public class MyGroupController {
     private final UserRepository userRepository;
     private final RatingService ratingService;
 
+
+
     @Autowired
     public MyGroupController(UserRepository userRepository,
                              RatingService ratingService) {
         this.userRepository = userRepository;
         this.ratingService = ratingService;
+    }
+
+    @GetMapping("/")
+    public String showMyGroup(Model model, Principal principal) {
+        return "myGroup";
     }
 
     @PostMapping("/saveRating")
