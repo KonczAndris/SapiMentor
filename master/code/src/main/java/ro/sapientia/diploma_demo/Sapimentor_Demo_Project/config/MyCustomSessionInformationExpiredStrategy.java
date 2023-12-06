3f04782a5a7781a40c7baec35575bac7dea23a24
Szotyori Csongor-Botond <szotyori.csongor@student.ms.sapientia.ro>
2023-12-07 01:55:40 +0200

    Update

--- master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/config/MyCustomSessionInformationExpiredStrategy.java
+++ master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/config/MyCustomSessionInformationExpiredStrategy.java
@@ -1,9 +1,6 @@
 package ro.sapientia.diploma_demo.Sapimentor_Demo_Project.config;
 
 import org.springframework.security.web.session.InvalidSessionStrategy;
-import org.springframework.security.web.session.SessionInformationExpiredEvent;
-import org.springframework.security.web.session.SessionInformationExpiredStrategy;
-import ro.sapientia.diploma_demo.Sapimentor_Demo_Project.model.User;
 import ro.sapientia.diploma_demo.Sapimentor_Demo_Project.repository.UserRepository;
 
 import javax.servlet.ServletException;
@@ -11,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class MyCustomSessionInformationExpiredStrategy implements InvalidSessionStrategy, SessionInformationExpiredStrategy {
+public class MyCustomSessionInformationExpiredStrategy implements InvalidSessionStrategy {
 
 
     private UserRepository userRepository;
