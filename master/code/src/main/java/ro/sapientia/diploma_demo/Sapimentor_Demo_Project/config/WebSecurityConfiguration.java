7b7dfd1f28a1e7db34844c7aeb8460fa361b7cda
Szotyori Csongor-Botond <szotyori.csongor@student.ms.sapientia.ro>
2024-03-06 00:51:57 +0200

    Update

--- master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/config/WebSecurityConfiguration.java
+++ master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/config/WebSecurityConfiguration.java
@@ -36,8 +36,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf()
-                .disable()
-                //.and()
+                //.disable()
+                .and()
                 .headers()
                 .frameOptions()
                 .sameOrigin()
