651d9459da367065552199b4677e8b7d931ae201
Szotyori Csongor-Botond <szotyori.csongor@student.ms.sapientia.ro>
2024-03-09 16:32:34 +0200

    Update
    --> Resolve CORS configuration issue.

--- master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/config/CrosConfig.java
+++ master/code/src/main/java/ro/sapientia/diploma_demo/Sapimentor_Demo_Project/config/CrosConfig.java
@@ -13,10 +13,11 @@ public class CrosConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("https://sapimentor.eu"); // Engedélyezett eredet
-        config.addAllowedHeader("*"); // Engedélyez minden fejlécet
+        config.addAllowedOrigin("https://sapimentor.eu");
+        config.addAllowedOrigin("https://www.sapimentor.eu");
+        config.addAllowedHeader("*");
         config.addAllowedMethod("POST");
-        config.addAllowedMethod("GET");// Engedélyezett kérési módszer
+        config.addAllowedMethod("GET");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
