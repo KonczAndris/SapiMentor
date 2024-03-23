d40000669c2abd41996dd6a3ad966f5be9bb7801
KonczAndris <koncz.andras@student.ms.sapientia.ro>
2024-03-23 15:16:44 +0200

    Update
    Dark Theme Update

--- master/code/target/classes/static/js/functions.js
+++ master/code/target/classes/static/js/functions.js
@@ -1101,7 +1101,7 @@ function notMenteeOrMentor() {
     alertModal.classList.add('alertModal');
     alertModal.innerHTML = `
         <div style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;  user-select: none;">
-            <div style="background-color: white; padding: 20px; border-radius: 25px; text-align: center;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            <div class="alertModal-content" style="background-color: white; padding: 20px; border-radius: 25px; text-align: center;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
                 -webkit-animation-name: animatetop;
                 -webkit-animation-duration: 0.4s;
                 animation-name: animatetop;
